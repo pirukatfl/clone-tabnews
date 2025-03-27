bcebc5e3e6c41383abee408baa2b72015efb783c
Thiago Furlan Linhares <iagokorn@yahoo.com.br>
2025-03-27 03:18:45 +0000

    add databse.js e .env_example

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,9 @@
 // /api/status
+import database from "../../../../infra/database.js";
 
-function status(request, response) {
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ msg: "Ronaldo" });
 }
 
